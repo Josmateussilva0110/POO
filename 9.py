367a3922896c8eec81823a1f4819d6c1838199c4
Mateus Silva <127849926+Josmateussilva0110@users.noreply.github.com>
2023-05-14 19:53:09 -0300

    Delete 9.py

--- 9.py
+++
@@ -1,9 +0,0 @@
-value = int(input('quantos valores: '))
-t1 = 0
-t2 = 1
-print(f'{t1} {t2}', end=' ')
-for i in range(3, value+1):
-    t3 = t1 + t2
-    print(f'{t3}',end=' ')
-    t1 = t2 
-    t2 = t3
